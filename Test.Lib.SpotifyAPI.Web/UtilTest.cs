d500c8f775fa761c5a90cc6e94edd7a2e91d4a5a
SimonG96 <simon.gockner@gmail.com>
2018-08-28 22:53:21 +0200

    - add Tests for Lib.SpotifyApi.Web

---
+++ Test.Lib.SpotifyAPI.Web/UtilTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Lib.SpotifyAPI.Web;
+using NUnit.Framework;
+
+namespace Test.Lib.SpotifyAPI.Web
+{
+    [TestFixture]
+    public class UtilTest
+    {
+        [Test]
+        public void TimestampShouldBeNoFloatingPoint()
+        {
+            string timestamp = DateTime.Now.ToUnixTimeMillisecondsPoly().ToString();
+
+            StringAssert.DoesNotContain(".", timestamp);
+            StringAssert.DoesNotContain(",", timestamp);
+        }
+    }
+}
