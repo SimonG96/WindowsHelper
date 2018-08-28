d5cdd13975f2e1bd7f9124a49f9c54584184e2f2
SimonG96 <simon.gockner@gmail.com>
2018-08-28 22:03:41 +0200

    - add spotify

---
+++ WindowsHelper/Spotify/Spotify.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using WindowsHelper.Events;
+using WindowsHelper.Interfaces;
+using WindowsHelper.Spotify.ViewModels;
+
+namespace WindowsHelper.Spotify
+{
+    public class Spotify : IPlugin
+    {
+        public Spotify()
+        {
+            Settings = new SpotifySettings(this);
+
+            ShowSpotifyWindowEvent.ShowSpotifyWindow += OnShowSpotifyWindow;
+        }
+
+        public string Name => nameof(Spotify);
+        public ISettings Settings { get; }
+
+
+        #region Methods
+
+        public bool Init()
+        {
+            return true;
+        }
+
+        public void DeInit()
+        {
+            
+        }
+
+        private void OnShowSpotifyWindow(object sender, EventArgs args)
+        {
+            SpotifyWindow spotifyWindow = new SpotifyWindow();
+            spotifyWindow.DataContext = new SpotifyWindowViewModel();
+            spotifyWindow.Show();
+        }
+
+
+        public void Dispose()
+        {
+            
+        }
+
+        #endregion Methods
+    }
+}
---
+++ WindowsHelper/Spotify/SpotifySettings.cs
@@ -0,0 +1,15 @@
+﻿using WindowsHelper.Interfaces;
+
+namespace WindowsHelper.Spotify
+{
+    public class SpotifySettings : ISettings
+    {
+        public SpotifySettings(object parent)
+        {
+            Parent = parent;
+        }
+
+        public string Name => "Spotify";
+        public object Parent { get; }
+    }
+}
