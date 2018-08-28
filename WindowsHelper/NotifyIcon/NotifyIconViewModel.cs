b6e90e52c44d39421e17b450293e30fc6e6dfbc2
SimonG96 <simon.gockner@gmail.com>
2018-08-28 22:02:19 +0200

    - add notify icon to windows helper

---
+++ WindowsHelper/NotifyIcon/NotifyIconViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Input;
+using WindowsHelper.Events;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace WindowsHelper.NotifyIcon
+{
+    public class NotifyIconViewModel : ViewModelBase
+    {
+        public ICommand ShowWindowCommand => new RelayCommand(ShowWindow);
+        public ICommand ShowSettingsWindowCommand => new RelayCommand(ShowSettingsWindow);
+        public ICommand ShowSpotifyWindowCommand => new RelayCommand(ShowSpotifyWindow);
+        public ICommand ExitCommand => new RelayCommand(Exit);
+
+
+        private void ShowWindow()
+        {
+            MainWindowEnabledEvent.RaiseMainWindowEnabledEvent(this, true);
+        }
+
+        private void ShowSettingsWindow()
+        {
+            ShowSettingsWindowEvent.RaiseShowSettingsWindowEvent(this);
+        }
+
+        private void ShowSpotifyWindow()
+        {
+            ShowSpotifyWindowEvent.RaiseShowSpotifyWindowEvent(this);
+        }
+
+        private void Exit()
+        {
+            CloseRequestedEvent.RaiseCloseRequestedEvent(this);
+        }
+    }
+}
