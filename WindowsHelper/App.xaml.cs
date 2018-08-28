9dd904be004ba51a782337be35a96dbb697eef04
SimonG96 <simon.gockner@gmail.com>
2018-08-28 22:05:44 +0200

    - add app.xaml

--- WindowsHelper/App.xaml.cs
+++ WindowsHelper/App.xaml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using Lib.NotifyIconWpf;
+using Lib.Tools;
 
 namespace WindowsHelper
 {
@@ -13,5 +9,20 @@ namespace WindowsHelper
     /// </summary>
     public partial class App : Application
     {
+        private TaskbarIcon _notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs args)
+        {
+            RegistryHelper.Initialize("WindowsHelper");
+
+            base.OnStartup(args);
+            _notifyIcon = (TaskbarIcon) FindResource("NotifyIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs args)
+        {
+            _notifyIcon.Dispose();
+            base.OnExit(args);
+        }
     }
 }
