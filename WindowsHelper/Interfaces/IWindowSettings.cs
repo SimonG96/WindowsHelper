6d5f639f40c9239855d9e7168dc2e13effdf2e81
SimonG96 <simon.gockner@gmail.com>
2018-09-01 14:58:50 +0200

    - add new interface IWindowSettings

---
+++ WindowsHelper/Interfaces/IWindowSettings.cs
@@ -0,0 +1,12 @@
+﻿using WindowsHelper.Settings;
+
+namespace WindowsHelper.Interfaces
+{
+    public interface IWindowSettings : ISettings
+    {
+        double WindowHeight { get; set; }
+        double WindowWidth { get; set; }
+        double WindowTop { get; set; }
+        double WindowLeft { get; set; }
+    }
+}
