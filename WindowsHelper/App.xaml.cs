f25d10e6a24e69eb43dc5ccdee0c80b96f23ec66
SimonG96 <simon.gockner@gmail.com>
2018-09-20 21:25:21 +0200

    -add exception handling and additional logging

--- WindowsHelper/App.xaml.cs
+++ WindowsHelper/App.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System.Windows;
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+using WindowsHelper.Common;
 using Lib.NotifyIconWpf;
 using Lib.Tools;
+using Lib.Tools.Logging;
 
 namespace WindowsHelper
 {
@@ -10,18 +15,39 @@ namespace WindowsHelper
     public partial class App : Application
     {
         private TaskbarIcon _notifyIcon;
+        private Log _log;
 
         protected override void OnStartup(StartupEventArgs args)
         {
-            RegistryHelper.Initialize("WindowsHelper");
+            try
+            {
+                _log = new Log();
+                Log.Write($"Windows Helper {Assembly.GetExecutingAssembly().GetName().Version}");
+                Log.Write($"Written by SimonG");
+                Log.Write("");
+                Log.Write($"OS: {Environment.OSVersion}");
+                Log.Write($"User: {Environment.UserName} @ {Environment.UserDomainName}");
 
-            base.OnStartup(args);
-            _notifyIcon = (TaskbarIcon) FindResource("NotifyIcon");
+                DispatcherUnhandledException += ExceptionHandler.OnDispatcherUnhandledException;
+                TaskScheduler.UnobservedTaskException += ExceptionHandler.OnUnobservedTaskException;
+                AppDomain.CurrentDomain.UnhandledException += ExceptionHandler.OnCurrentDomainUnhandledException;
+
+                RegistryHelper.Initialize("WindowsHelper");
+                _notifyIcon = (TaskbarIcon) FindResource("NotifyIcon");
+
+                base.OnStartup(args);
+            }
+            catch (Exception ex)
+            {
+                Log.Write(ex);
+                ExceptionHandler.HandleException(ex);
+            }
         }
 
         protected override void OnExit(ExitEventArgs args)
         {
             _notifyIcon.Dispose();
+            _log.Dispose();
             base.OnExit(args);
         }
     }
--- WindowsHelper/Common/ExceptionHandler.cs
+++ WindowsHelper/Common/ExceptionHandler.cs
@@ -1,7 +1,45 @@
-﻿namespace WindowsHelper.Common
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using WindowsHelper.ToastNotification.Common;
+using WindowsHelper.ToastNotification.Enums;
+using Lib.Tools.Logging;
+
+namespace WindowsHelper.Common
 {
-    public class ExceptionHandler
+    public static class ExceptionHandler
     {
-        
+        public static void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs args) //TODO: set handled?
+        {
+            Log.Write(args.Exception);
+            Toast.ShowError(args.Exception, ToastBehaviour.TimedLong);
+        }
+
+        public static void OnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs args) //TODO: set observed?
+        {
+            Log.Write(args.Exception);
+            Toast.ShowError(args.Exception, ToastBehaviour.TimedLong);
+        }
+
+        public static void OnCurrentDomainUnhandledException(object sender, UnhandledExceptionEventArgs args) //TODO: handle isTerminating
+        {
+            if (args.ExceptionObject is Exception exception)
+            {
+                Log.Write(exception);
+                Toast.ShowError(exception, ToastBehaviour.TimedLong);
+            }
+            else
+            {
+                string message = "An unhandled Exception occurred!";
+                Log.Write(message, LogLevel.Error);
+                Toast.ShowError(message, ToastBehaviour.TimedLong);
+            }
+        }
+
+        public static void HandleException(Exception exception)
+        {
+            Log.Write(exception);
+            Toast.ShowError(exception, ToastBehaviour.TimedLong); //TODO: Choose when to use which behaviour
+        }
     }
 }
