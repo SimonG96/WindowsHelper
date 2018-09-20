87e45a69250f5a7eb3483611c5a58dc580019a75
SimonG96 <simon.gockner@gmail.com>
2018-09-20 21:22:38 +0200

    - update toastWindow

--- WindowsHelper/ToastNotification/ToastWindow.xaml.cs
+++ WindowsHelper/ToastNotification/ToastWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading;
+using System.Windows;
 
 namespace WindowsHelper.ToastNotification
 {
@@ -7,20 +9,32 @@ namespace WindowsHelper.ToastNotification
     /// </summary>
     public partial class ToastWindow : Window
     {
-        public ToastWindow(ToastWindowViewModel dataContext)
+        public ToastWindow()
         {
             InitializeComponent();
-            DataContext = dataContext;
-            ContentControl.Content = dataContext;
+
+            MouseEnter += delegate { MouseOverChanged?.Invoke(this, true); };
+            MouseLeave += delegate { MouseOverChanged?.Invoke(this, false); };
         }
+
+        public Timer Timer { get; set; }
+
 
         #region Methods
 
         private void OnClose(object sender, RoutedEventArgs args)
         {
+            Timer?.Dispose();
             Close();
         }
 
         #endregion
+
+
+        #region Events
+
+        public event EventHandler<bool> MouseOverChanged;
+
+        #endregion Events
     }
 }
