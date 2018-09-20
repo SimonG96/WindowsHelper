b534fb01a792c44c0a93b48809fec08db71f4531
SimonG96 <simon.gockner@gmail.com>
2018-09-20 21:21:08 +0200

    - update toast template selector

--- WindowsHelper/ToastNotification/Common/ToastTemplateSelector.cs
+++ WindowsHelper/ToastNotification/Common/ToastTemplateSelector.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using WindowsHelper.ToastNotification.Enums;
+using WindowsHelper.Interfaces;
+using WindowsHelper.ToastNotification.ViewModels;
 
 namespace WindowsHelper.ToastNotification.Common
 {
@@ -18,22 +19,19 @@ namespace WindowsHelper.ToastNotification.Common
             if (item == null)
                 return null;
 
-            if (!(item is  ToastWindowViewModel toastWindowViewModel))
+            if (!(item is IToast toast))
                 throw new InvalidOperationException("Item is not a ToastWindowViewModel");
 
-            switch (toastWindowViewModel.Type)
-            {
-                case ToastType.Info:
-                    return InfoTemplate;
-                case ToastType.Warning:
-                    return WarningTemplate;
-                case ToastType.Error:
-                    return ErrorTemplate;
-                case ToastType.Spotify:
-                    return SpotifyTemplate;
-                default:
-                    throw new NotImplementedException(); //TODO: What to do in this case?
-            }
+            if (toast is InfoToastViewModel)
+                return InfoTemplate;
+            else if (toast is WarningToastViewModel)
+                return WarningTemplate;
+            else if (toast is ErrorToastViewModel)
+                return ErrorTemplate;
+            else if (toast is SpotifyToastViewModel)
+                return SpotifyTemplate;
+            else
+                throw new NotImplementedException(); //TODO: What to do in this case?
         }
     }
 }
